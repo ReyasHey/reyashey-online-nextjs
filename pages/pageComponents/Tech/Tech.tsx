d5512a384b233c5ce440dabd167630c2a6a7b4bb
ReyasHey <reyas.hey@gmail.com>
2023-11-11 20:56:45 +0100

    Update: Changed rectangles in bg of Tech to have no opacity

--- pages/pageComponents/Tech/Tech.tsx
+++ pages/pageComponents/Tech/Tech.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image';
 import SectionTitle from '../../generalComponents/SectionTitle/SectionTitle'
 import Technology from './Technology/Technology'
 
-import recs from "../../../public/imgs/TechRectangles/rectangle-blood-dragon.svg"
+// import recs from "../../../public/imgs/TechRectangles/rectangle-blood-dragon.svg"
+import recs from "../../../public/imgs/TechRectangles/rectangle-blood-dragon-no-opacity.svg"
+
 
 function Tech() {
   return (
@@ -10,24 +12,25 @@ function Tech() {
         <Image
             src={ recs }
             alt="Decorative Rectangle 1"
-            style={{ width: "100%", zIndex: "-1000000", marginTop: "-6rem", position: "relative", filter: "brightness(98%)" }}
+            style={{ width: "100%", zIndex: "-1000000", marginTop: "-5rem", position: "relative", filter: "brightness(98%)" }}
         />
 
-        <SectionTitle title="Tech" className="mt-3 mb-2" />
-
-        <ul className='mt-3 row col-lg-9 col-11 mx-auto' style={{ height: "5rem" }}>
-          <Technology text="Html5" />
-          <Technology text=".NET" />
-          <Technology text="MySQL" />
-          <Technology text="React" />
-          <Technology text="Next.js" />
-          <Technology text="and More..." />
-        </ul>
+        <div style={{ padding: "3vw" }}>
+          <SectionTitle title="Tech" className="mt-3 mb-2" />
+          <ul className='mt-4 row col-lg-9 col-11 mx-auto' style={{ height: "5rem" }}>
+            <Technology text="Html5" />
+            <Technology text=".NET" />
+            <Technology text="MySQL" />
+            <Technology text="React" />
+            <Technology text="Next.js" />
+            <Technology text="and More..." />
+          </ul>
+        </div>
 
         <Image
             src={ recs }
             alt="Decorative Rectangle 1"
-            style={{ width: "100%", zIndex: "-1000000", marginBottom: "-6rem", position: "relative", transform: "rotate(180deg)", filter: "brightness(98%)" }}
+            style={{ width: "100%", zIndex: "-1000000", marginBottom: "-5rem", position: "relative", transform: "rotate(180deg)", filter: "brightness(98%)" }}
         />
     </section>
   )
