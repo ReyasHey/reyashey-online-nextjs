0f381cfaa32c510aa58cf995131a16e0ea4ad385
ReyasHey <reyas.hey@gmail.com>
2023-11-11 23:49:13 +0100

    Fix: Gtag consent on first visit

--- logic/gtag.ts
+++ logic/gtag.ts
@@ -3,7 +3,7 @@ import ReactGA from "react-ga4";
 
 export function InitGtag(){
 
-    if(Cookies.get("gtagConsentGranted") != "true")
+    if(Cookies.get("gtagConsentGranted") == "false")
         return false
 
     if (process.env.NODE_ENV !== "production")
