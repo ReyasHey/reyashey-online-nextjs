e7fba1df42b146bb5af189a5c6b74dae517f43d5
ReyasHey <reyas.hey@gmail.com>
2023-11-14 21:23:34 +0100

    Update: Optimization loading Bootstrap CDN

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { useState } from 'react';
+
 
 export default function Document() {
+
+const [bootstrapRel, setBootstrapRel] = useState<string>("stylesheet")
+
+  function handleOnLoadBootstrap (){
+    setBootstrapRel("stylesheet")
+  }
+
   return (
     <Html lang='en'>
       <Head>
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css' />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel={bootstrapRel} as="style" onLoad={() => handleOnLoadBootstrap()}/>
+        {/* <link href="main.css" rel="preload" as="style" onLoad={() => this.rel='stylesheet'} /> */}
+
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="canonical" href="https://reyashey.com" />
       </Head>
