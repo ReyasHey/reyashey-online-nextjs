46d1a1b2d7baf8c4ff5d84b51952b534003fb177
ReyasHey <reyas.hey@gmail.com>
2023-12-17 17:25:49 +0100

    Feat(TrustPilot Verification): Added tmp meta tag for domain verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ const [bootstrapRel, setBootstrapRel] = useState<string>("stylesheet")
       <Head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel={bootstrapRel} as="style" onLoad={() => handleOnLoadBootstrap()}/>
         {/* <link href="main.css" rel="preload" as="style" onLoad={() => this.rel='stylesheet'} /> */}
-
+        <meta name="trustpilot-one-time-domain-verification-id" content={process.env.trustpilot_one_time_domain_verification_id}/>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <link rel="canonical" href="https://reyashey.com" />
       </Head>
